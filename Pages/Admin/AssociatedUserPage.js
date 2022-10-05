8fb26b189ccb929e647997a32a3f71df8d9b8f73
MJ Mayar <wahidullahmayar2020@gmail.com>
2022-10-05 16:30:47 -0500

    AssociatedUser TestCase

--- Pages/Admin/AssociatedUserPage.js
+++ Pages/Admin/AssociatedUserPage.js
@@ -6,14 +6,14 @@ class AssociatedUserPage {
        // this.AssocUserPage=page.locator("h2[xpath='1']"); We are not sure about this one
         this.pageTable=page.locator(".col-sm-6"); //the complete table on that page
         this.firstColHeaderVal=page.locator("th[data-field='1']");
-        this.secondColHeaderVal=page.locator("text=Name");
-        this.thirdColHeaderVal=page.locator("tex=Email");
+        this.secondColHeaderVal=page.locator("//div[normalize-space()='Name']");
+        this.thirdColHeaderVal=page.locator("//div[normalize-space()='Email']");
         this.addBtn=page.locator("#UserAddButton").last();
         this.deletBtn=page.locator("button[id='UserDeleteButton']");
     }
 
     async openAdminPage(){
-       await this.page.navigate("https://app-test.ipfs.com/Core/Admin/AssociatedUser")
+       await this.page.goto("https://app-test.ipfs.com/Core/Admin/AssociatedUser")
     }
 
     getMainTextFrmPage(){
@@ -31,7 +31,7 @@ class AssociatedUserPage {
 
     getThirdHeaderLoc()
     {
-        this.thirdColHeaderVal
+        return this.thirdColHeaderVal
     }
 
     getAddBtnLoc(){
--- tests/Admin/AssociatedUserTest.spec.js
+++ tests/Admin/AssociatedUserTest.spec.js
@@ -1,23 +1,26 @@
-const { test, expect } = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 const {AssociatedUserPage} = require("../../Pages/Admin/AssociatedUserPage");
+const {LoginPage} = require("../../Pages/LoginPage");
 
 
+test('@ipfs @regression @admin Verify page contents', async ({browser}) => {
 
-test('@ipfs @regression @admin Verify page contents', async ({ browser }) => {
-
-  let aup;
-    const context=await browser.newContext({
-        storageState:"auth.jason"
+    let aup;
+    const context = await browser.newContext({
+        storageState: "auth.jason"
     })
-    const  page=await context.newPage();
-
-     aup= new AssociatedUserPage(page);
-     await expect(aup.getMainTextFrmPage()).toHaveText("Associated Users")
-    await  expect(aup.getFirstColHeader()).toHaveText("Login");
-    await  expect(aup.getSecondHeaderLoc()).toHaveText("Name");
-    await  expect(aup.getThirdHeaderLoc()).toHaveText("Email");
-    await  expect(aup.getAddBtnLoc()).toHaveText("Add");
-    await  expect(aup.getDeleteBtnLoc()).toHaveText("Delete");
+    const page = await context.newPage();
+    aup = new AssociatedUserPage(page);
+    const loginPage = new LoginPage(page);
+    await loginPage.login("pfsit", "pfs1ttest");
+    await page.waitForLoadState('networkidle');
+    await aup.openAdminPage();
+    await expect(aup.getMainTextFrmPage()).toHaveText("Associated Users")
+    await expect(aup.getFirstColHeader()).toHaveText("Login");
+    await expect(aup.getSecondHeaderLoc()).toHaveText("Name");
+    await expect(aup.getThirdHeaderLoc()).toHaveText("Email");
+    await expect(aup.getAddBtnLoc()).toHaveText("Add");
+    await expect(aup.getDeleteBtnLoc()).toHaveText("Delete");
 
 
 });
