ddaf27e6370312ad009abd3e8f5b9420a92e3aeb
saimanazir <annajammes9@gmail.com>
2022-10-10 22:50:27 +0500

    entitySetting Commit

--- tests/Admin/EntitySettings.spec.js
+++ tests/Admin/EntitySettings.spec.js
@@ -5,15 +5,29 @@ const {LoginPage} = require("../../Pages/LoginPage");
 test.beforeAll(async({browser})=>
 {
     let page = await browser.newPage();
-    entitySettingsPage = await new EntitySettingsPage(page);
+  const  entitySettingsPage = await new EntitySettingsPage(page);
     const loginPage =await  new LoginPage(page);
     await loginPage.login();
     await page.waitForLoadState('networkidle');
-    await entitySettingsPage.openEntitySettingPage();
+    await entitySettingsPage.openEntitySettingsPage();
 
 });
 
 test.only("entity setting page test case",async (page)=>{
 await entitySettingsPage.PFFeeVerb.click();
+await entitySettingsPage.PFFeeVerb.selectOption("Reimbursement");
+await entitySettingsPage.PFFeeDiscNonCA.click();
+await entitySettingsPage.PFFeeDiscNonCA.selectOption("Letter");
+await entitySettingsPage.PFFeeDiscCA.click();
+await entitySettingsPage.PFFeeDiscCA.selectOption("Letter");
+await entitySettingsPage.weleMailToInsChkBox.click();
+await entitySettingsPage.saveBtn.click();
+await entitySettingsPage.cancelBtn.click();
+await entitySettingsPage.FaceLink.click();
+const [newWindow]=await entitySettingsPage.TwitLink.click();
+console.log(newWindow.url());
+
+
+
 
 });
